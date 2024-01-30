7cd0ec92c59943de00b8f30b76a92e1d1f5ae244
donjuanplatinum <donjuan@donjuan.lecturify.net>
2024-01-30 20:48:28 +0800

    mod.rs

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,4 +1,6 @@
 mod square;
-
+///矩阵
+mod matrix;
 pub use self::square::add as square_matrix_add;
 pub use self::square::multiply as square_matrix_multiply;
+pub use self::matrix::Matrix as Matrix;
