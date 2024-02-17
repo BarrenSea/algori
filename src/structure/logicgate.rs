45b9a0e169123934c829a48e7818bc10333e94ff
donjuanplatinum <donjuan@lecturify.net>
2024-02-17 20:01:41 +0800

    Halfadder,DelayLine

--- src/structure/logicgate.rs
+++ src/structure/logicgate.rs
@@ -1,3 +1,5 @@
+
+
 ///逻辑门
 pub trait LogicGate {
     fn get_result(&self) -> bool;
@@ -156,5 +158,165 @@ pub struct LowLevel{
 impl LogicGate for LowLevel {
     fn get_result(&self) -> bool {
 	false
+    }
+}
+
+///异或门 输入不一样true
+/// # Examples
+/// ```
+/// use algori::structure::Xor;
+/// use algori::structure::LogicGate;
+/// let a = Xor{input1:true,input2:false};
+/// let b = Xor{input1:true,input2:true};
+/// let c = Xor{input1:false,input2:false};
+/// assert_eq!(a.get_result(),true);
+/// assert_eq!(b.get_result(),false);
+/// assert_eq!(c.get_result(),false);
+/// ```
+pub struct Xor{
+    pub input1: bool,
+    pub input2: bool,
+}
+impl LogicGate for Xor{
+    fn get_result(&self) ->bool {
+	let a:And = And{input1:self.input1,input2:self.input2};
+	let b:Nor = Nor{input1:self.input1,input2:self.input2};
+	let c:Nor = Nor{input1:a.get_result(),input2:b.get_result()};
+	c.get_result()
+    }
+}
+
+
+///三路或门
+/// # Examples
+/// ```
+/// use algori::structure::ThreeOr;
+/// use algori::structure::LogicGate;
+/// let a = ThreeOr{input1:true,input2:false,input3:false};
+/// let b = ThreeOr{input1:false,input2:true,input3:false};
+/// let c = ThreeOr{input1:false,input2:false,input3:true};
+/// let d = ThreeOr{input1:false,input2:false,input3:false};
+/// assert_eq!(a.get_result(),true);
+/// assert_eq!(b.get_result(),true);
+/// assert_eq!(c.get_result(),true);
+/// assert_eq!(d.get_result(),false);
+/// ```
+pub struct ThreeOr{
+    pub input1: bool,
+    pub input2: bool,
+    pub input3: bool,
+}
+
+impl LogicGate for ThreeOr{
+    fn get_result(&self) ->bool {
+	let a:Or = Or{input1:self.input1,input2:self.input2};
+	let b:Or = Or{input1:self.input2,input2:self.input3};
+	let c:Or = Or{input1:a.get_result(),input2:b.get_result()};
+	c.get_result()
+    }
+}
+
+
+///三路与门
+/// # Examples
+/// ```
+/// use algori::structure::ThreeAnd;
+/// use algori::structure::LogicGate;
+/// let a = ThreeAnd{input1:true,input2:false,input3:false};
+/// let b = ThreeAnd{input1:false,input2:true,input3:false};
+/// let c = ThreeAnd{input1:false,input2:false,input3:true};
+/// let d = ThreeAnd{input1:false,input2:false,input3:false};
+/// let e = ThreeAnd{input1:true,input2:true,input3:true};
+/// assert_eq!(a.get_result(),false);
+/// assert_eq!(b.get_result(),false);
+/// assert_eq!(c.get_result(),false);
+/// assert_eq!(d.get_result(),false);
+/// assert_eq!(e.get_result(),true);
+/// ```
+pub struct ThreeAnd{
+    pub input1: bool,
+    pub input2: bool,
+    pub input3: bool,
+}
+
+impl LogicGate for ThreeAnd{
+    fn get_result(&self) ->bool {
+	let a:And = And{input1:self.input1,input2:self.input2};
+	let b:And = And{input1:self.input2,input2:self.input3};
+	let c:And = And{input1:a.get_result(),input2:b.get_result()};
+	c.get_result()
+    }
+}
+///同或门
+///相同则为true
+/// # Examples
+/// ```
+/// use algori::structure::Xnor;
+/// use algori::structure::LogicGate;
+/// let a = Xnor{input1:true,input2:false};
+/// let b = Xnor{input1:true,input2:true};
+/// let c = Xnor{input1:false,input2:false};
+/// assert_eq!(a.get_result(),false);
+/// assert_eq!(b.get_result(),true);
+/// assert_eq!(c.get_result(),true);
+/// ```
+pub struct Xnor{
+    pub input1: bool,
+    pub input2: bool,
+}
+
+impl LogicGate for Xnor{
+    fn get_result(&self) ->bool {
+	let a:Xor = Xor{input1:self.input1,input2:self.input2};
+	let b:Not = Not{input: a.get_result()};
+	b.get_result()
+    }
+}
+
+use std::time::Duration;
+///延迟线
+///Examples
+///```
+/// use algori::structure::DelayLine;
+/// let a = DelayLine{delay: 20,input: true};
+/// 
+/// a.get_result();
+///```
+
+pub struct DelayLine {
+    /// 延迟时间，单位为毫秒
+    pub delay: u64,
+    pub input: bool,
+}
+
+impl DelayLine {
+    pub fn get_result(&self) -> bool {
+        std::thread::sleep(Duration::from_millis(self.delay));
+        self.input
+    }
+}
+
+///半加器
+///输出一个包含低位结果与进位的元组
+/// Examples
+///```
+///use algori::structure::HalfAdder;
+///let a:HalfAdder = HalfAdder{input1:true,input2:false};
+///assert_eq!(a.get_result(),(true,false));
+///let a:HalfAdder = HalfAdder{input1:true,input2:true};
+///assert_eq!(a.get_result(),(false,true));
+pub struct HalfAdder {
+    pub input1: bool,
+    pub input2: bool,
+}
+
+
+
+impl HalfAdder{
+    ///返回(sum,carry)
+    pub fn get_result(&self) -> (bool,bool) {
+	let a:Xor = Xor{input1: self.input1,input2:self.input2};
+	let b:And = And{input1: self.input1,input2:self.input2};
+	(a.get_result(),b.get_result())
     }
 }
