5d1ad8bc5e9ec680e90ffb67d36365fefdba1f21
donjuanplatinum <donjuan@lecturify.net>
2024-02-27 23:57:21 +0800

    heap_sort

--- src/sort/heap.rs
+++ src/sort/heap.rs
@@ -9,28 +9,28 @@
 ///let c = heap_max_sort(&mut a);
 ///assert_eq!(a,[1,3,4,5,6,6,7,9,65,65]);
 ///```
-fn max<T: Ord>(arr: &mut [T], n: usize, mut i: usize) {
+fn max<T: Ord>(arr: &mut [T], i: usize) {
+    let mut n: usize = i ;
     //循还下滤
     loop {
-	//当前结点下标
-        let mut largest:usize = i;
+        let mut largest:usize = n;
 	//左子结点
-        let left:usize = 2 * i + 1;
+        let left:usize = 2 * n + 1;
 	//右子结点
-        let right:usize = 2 * i + 2;
+        let right:usize = 2 * n + 2;
 	//左子结点大于目前结点则交换下标
-        if left < n && arr[left] > arr[largest] {
+        if left < arr.len() && arr[left] > arr[largest] {
             largest = left;
         }
 	//右子结点大于目前结点则交换下标
-        if right < n && arr[right] > arr[largest] {
+        if right < arr.len() && arr[right] > arr[largest] {
             largest = right;
         }
 	//到这里则对一个结点及其子结点的下标排好了序
 	//若下标发生了变换 则交换父结点与最大的子结点
-        if largest != i {
-            arr.swap(i, largest);
-            i = largest;
+        if largest != n {
+            arr.swap(n, largest);
+            n = largest;
         } else {
 	    //下滤完成
             break;
@@ -44,66 +44,17 @@ fn build_max<T: Ord>(arr: &mut [T]) {
     let n = arr.len();
     // n/2为拥有子结点的下标最大的
     for i in (0..n / 2).rev() {
-        max(arr, n, i);
+        max(arr, i);
     }
 }
 ///大根堆排序
 ///
 ///获取一个可变引用并排序
-pub fn max_sort<T: Ord>(arr: &mut [T]) {
+pub fn heap_sort<T: Ord>(arr: &mut [T]) {
     let n = arr.len();
     build_max(arr);
-
     for i in (0..n).rev() {
         arr.swap(0, i);
-        max(arr, i, 0);
+        max(&mut arr[..i], 0);
     }
 }
-
-
-/// 上滤
-/// 复杂度为O(logn)
-fn min<T: Ord>(arr: &mut [T], n: usize, mut i: usize) {
-    loop {
-        let mut smallest = i;
-        let left = 2 * i + 1;
-        let right = 2 * i + 2;
-
-        if left < n && arr[left] < arr[smallest] {
-            smallest = left;
-        }
-
-        if right < n && arr[right] < arr[smallest] {
-            smallest = right;
-        }
-
-        if smallest != i {
-            arr.swap(i, smallest);
-            i = smallest;
-        } else {
-            break;
-        }
-    }
-}
-
-fn build_min<T: Ord>(arr: &mut [T]) {
-    let n = arr.len();
-    for i in (0..n / 2).rev() {
-        min(arr, n, i);
-    }
-}
-
-/// 小根堆排序
-///
-/// 获取一个可变引用并排序
-pub fn min_sort<T: Ord>(arr: &mut [T]) {
-    let n = arr.len();
-    build_min(arr);
-
-    for i in (0..n).rev() {
-        arr.swap(0, i);
-        min(arr, i, 0);
-    }
-}
-
-
