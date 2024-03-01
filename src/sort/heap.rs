57feb0f0748091200f4af15a44ca20c2ba5dd76c
donjuanplatinum <donjuan@lecturify.net>
2024-03-01 23:29:45 +0800

    更改最大优先队列

--- src/sort/heap.rs
+++ src/sort/heap.rs
@@ -2,59 +2,68 @@
 ///复杂度为O(logn)
 ///# Examples
 ///```
-///
-///let mut  a  = [7,3,5,1,9,65,65,4,6,6];
-///
-///use algori::sort::heap_max_sort;
-///let c = heap_max_sort(&mut a);
-///assert_eq!(a,[1,3,4,5,6,6,7,9,65,65]);
+/// use algori::structure::MaxPriQueue;
+/// let a = MaxPriQueue::<i32>::new();
+/// let mut queue = MaxPriQueue::<i32>::new();
+/// queue.insert(5);
+/// queue.insert(3);
+/// queue.insert(7);
+/// queue.insert(9);
+/// queue.insert(8);
+/// assert_eq!(*queue.max().unwrap(), 9);
+/// queue.increase_key(10,4);
+/// assert_eq!(*queue.max(),unwrap,10);
 ///```
-fn max<T: PartialOrd>(arr: &mut [T], i: usize) {
-    let mut n: usize = i ;
-    //循还下滤
-    loop {
-        let mut largest:usize = n;
-	//左子结点
-        let left:usize = 2 * n + 1;
-	//右子结点
-        let right:usize = 2 * n + 2;
-	//左子结点大于目前结点则交换下标
-        if left < arr.len() && arr[left] > arr[largest] {
-            largest = left;
-        }
-	//右子结点大于目前结点则交换下标
-        if right < arr.len() && arr[right] > arr[largest] {
-            largest = right;
-        }
-	//到这里则对一个结点及其子结点的下标排好了序
-	//若下标发生了变换 则交换父结点与最大的子结点
-        if largest != n {
-            arr.swap(n, largest);
-            n = largest;
-        } else {
-	    //下滤完成
-            break;
-        }
-    }
+
+///最大优先队列
+pub struct MaxPriQueue<T: PartialOrd>{
+    heap: Vec<T>,
 }
 
-//建堆
-pub fn build_max<T: PartialOrd>(arr: &mut [T]) {
-
-    let n = arr.len();
-    // n/2为拥有子结点的下标最大的
-    for i in (0..n / 2).rev() {
-        max(arr, i);
+impl<T: PartialOrd> MaxPriQueue<T> {
+    ///返回一个最大优先队列
+    pub fn new() -> Self {
+	MaxPriQueue{heap: Vec::new()}
     }
-}
-///大根堆排序
-///
-///获取一个可变引用并排序
-pub fn heap_sort<T: PartialOrd>(arr: &mut [T]) {
-    let n = arr.len();
-    build_max(arr);
-    for i in (0..n).rev() {
-        arr.swap(0, i);
-        max(&mut arr[..i], 0);
+    ///返回最大堆的最大值,不获得所有权
+    pub fn max(&self) ->Result<&T,&str> {
+	if self.heap.len() < 1 {
+	    return Result::Err("heap underflow");
+	}
+	Ok(&self.heap[0])
+    }
+    ///弹出第一个元素,获得所有权
+    pub fn extract(&mut self) ->Result<T,&str> {
+	if self.heap.is_empty() {
+	    return Result::Err("heap underflow");
+	}
+	//弹出第一个元素
+	let max = Ok(self.heap.swap_remove(0));
+	if !self.heap.is_empty() {
+	    algori::sort::build_max(&mut self.heap);
+	}
+	max
+    }
+    ///更改下标为index的结点的key
+    pub fn increase_key(&mut self,key:T,index: usize) -> Result<(),&str>{
+	if key < self.heap[index]{
+	    return Err("The key is smaller than old");
+	}
+	self.heap[index] = key;
+	let mut index = index;
+	while index > 0 && self.heap[(index - 1) / 2] < self.heap[index] {
+	    self.heap.swap(index,(index - 1) / 2);
+	    index = (index + 1) / 2 - 1;
+	}
+	Ok(())
+    }
+    ///插入元素
+    pub fn insert(&mut self, key: T) ->() {
+	self.heap.push(key);
+	let mut index = self.heap.len() - 1;
+	while index > 0 && self.heap[(index - 1) / 2] < self.heap[index] {
+	    self.heap.swap(index,(index - 1) / 2);
+	    index = (index + 1) / 2 - 1;
+	}
     }
 }
