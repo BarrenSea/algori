b61ead150bf2995d4035aa69002b93fa56885c03
donjuanplatinum <donjuan@lecturify.net>
2024-04-21 20:10:31 +0800

    DFT in f64

--- src/math/DFT.rs
+++ src/math/DFT.rs
@@ -13,19 +13,18 @@ use std::ops::{Add, Sub, Mul};
 /// ```
 
 use crate::structure::Complex;
-pub fn dft<T: Copy + Default + std::convert::From<f64>>(signal: &[T]) -> Vec<Complex<T>> 
-where
-    T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Copy + Into<f64>
+pub async fn dft(signal: &[f64]) -> Vec<Complex<f64>>
+
 {
     let n = signal.len();
-    let mut spectrum = vec![Complex::new(T::default(), T::default()); n];
+    let mut spectrum = vec![Complex::new(f64::default(), f64::default()); n];
 
     for k in 0..n {
-        let mut sum = Complex::new(T::default(), T::default());
+        let mut sum = Complex::new(f64::default(), f64::default());
         for t in 0..n {
-            let angle = -2.0 * std::f64::consts::PI * k as f64 * t as f64 / n as f64;
-            let c = Complex::new(angle.cos().into(), angle.sin().into());
-            let value = Complex::new(signal[t], T::default());
+            let angle = -2.0 * std::f64::consts::PI * k as f64  * t as f64 / n as f64;
+            let c = Complex::new(angle.cos(), angle.sin());
+            let value = Complex::new(signal[t], f64::default());
             sum = sum + value * c;
         }
         spectrum[k] = sum;
