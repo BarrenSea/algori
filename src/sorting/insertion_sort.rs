8d18668c7f17b5d3d46dfc33646e4f3f32e0449d
donjuanplatinum <donjuan@lecturify.net>
2024-06-07 07:30:34 +0800

    1. 添加选择排序 2. 修改插入排序的接口 3. 添加文档

--- src/sorting/insertion_sort.rs
+++ src/sorting/insertion_sort.rs
@@ -1,25 +1,15 @@
-/// # Insertion_Sort
-pub fn insertion_sort<T>(array: &mut [T]) -> ()
-where
-    T: PartialOrd,
-{
+/// # Insertion Sort
+/// # Example
+/// ```
+/// use algori::sorting::{insertion_sort,is_sorted};
+/// let mut a = [2,3,1,34,15,8,0,7,4,3,21,4,6,7,4,2341,321,41231,312,62];
+/// insertion_sort(&mut a,|a,b| a <= b);
+/// is_sorted(&mut a,|a,b| a<=b);
+/// ```
+pub fn insertion_sort<T>(array:&mut [T],comparator: fn(&T,&T) -> bool)-> () {
     for point in 1..array.len() {
         let mut current_point: usize = point;
-        while current_point > 0 && array[current_point] < array[current_point - 1] {
-            array.swap(current_point, current_point - 1);
-            current_point -= 1;
-        }
-    }
-}
-
-/// # Reverse Insertion_Sort
-pub fn reverse_insertion_sort<T>(array: &mut [T]) -> ()
-where
-    T: PartialOrd,
-{
-    for point in 1..array.len() {
-        let mut current_point: usize = point;
-        while current_point > 0 && array[current_point] > array[current_point - 1] {
+        while current_point > 0 && comparator(&array[current_point],&array[current_point - 1]){
             array.swap(current_point, current_point - 1);
             current_point -= 1;
         }
@@ -29,11 +19,11 @@ where
 #[cfg(test)]
 mod insertion_sort_tests {
     use super::super::is_sorted;
-    use super::{insertion_sort, reverse_insertion_sort};
+    use super::insertion_sort;
     #[test]
     fn empty() -> () {
         let mut a: [i32; 0] = [];
-        insertion_sort(&mut a);
+        insertion_sort(&mut a,|a,b| a <=b);
 
         assert_eq!(is_sorted(&mut a, |a, b| a <= b), true);
     }
@@ -41,19 +31,19 @@ mod insertion_sort_tests {
     #[test]
     fn one_element() -> () {
         let mut a: [i32; 1] = [1];
-        insertion_sort(&mut a);
+        insertion_sort(&mut a,|a,b| a <=b);
         assert_eq!(is_sorted(&mut a, |a, b| a <= b), true);
     }
     #[test]
     fn positive() -> () {
         let mut a = [1, 123, 123, 12, 4234, 42, 1123, 123, 15112, 312];
-        insertion_sort(&mut a);
+        insertion_sort(&mut a,|a,b| a <=b);
         assert_eq!(is_sorted(&mut a, |a, b| a <= b), true);
     }
     #[test]
     fn reverse() -> () {
         let mut a = [1, 123, 123, 12, 4234, 42, 1123, 123, 15112, 312];
-        reverse_insertion_sort(&mut a);
+        insertion_sort(&mut a,|a,b| a >=b);
         assert_eq!(is_sorted(&mut a, |a, b| a >= b), true);
     }
 }
--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -1,5 +1,7 @@
 mod insertion_sort;
 mod utils;
+mod selection_sort;
 
 pub use self::insertion_sort::*;
 pub use self::utils::*;
+pub use self::selection_sort::*;
