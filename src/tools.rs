05addc058b832a36fe5715c0fc14793acaca82ad
donjuanplatinum <donjuan@lecturify.net>
2024-06-29 08:25:23 -0400

    修改macro

--- src/tools.rs
+++ src/tools.rs
@@ -1,16 +1,15 @@
-use std::time::{Duration, Instant};
-
 /// test the time of function
 /// # Examples
 /// ```
 /// use algori::test_time;
 /// use algori::sorting::insertion_sort;
 /// let mut a = [1,4,6,7,2,2,1,4,65,6];
-/// test_time!(insertion_sort(&mut a));
+/// test_time!(insertion_sort(&mut a,|a,b|a<=b));
 /// ```
 #[macro_export]
 macro_rules! test_time {
     ($func:expr) => {
+        use std::time::{Duration, Instant};
         let now = Instant::now();
         $func;
         println!(
